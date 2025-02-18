090b62aca70ceff88ad01a7e244ead8d0b1192f9
Kamila Nitta <kamila.nittarauscher@adevinta.com>
2025-02-18 20:25:40 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="weather-app">
+      <header>
+        <form id="search-form">
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            required
+            className="search-input"
+            id="search-input"
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="search-button"
+          />
+        </form>
+      </header>
+      <main>
+        <div className="current-weather">
+          <div>
+            <h1
+              className="current-city"
+              id="current-city"
+            >
+              Paris
+            </h1>
+            <p className="current-details">
+              <span id="current-date"></span>, moderate rain <br />
+              Humidity: <strong>87%</strong>, Wind: <strong>7.2km/h</strong>
+            </p>
+          </div>
+          <div className="current-temperature">
+            <span className="current-temperature-icon">☀️</span>
+            <span
+              className="current-temperature-value"
+              id="current-temperature"
+            >
+              24
+            </span>
+            <span className="current-temperature-unit">°C</span>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <p>
+          This project was coded by
+          <a
+            href="#"
+            target="_blank"
+          >
+            Kamila Nitta
+          </a>
+          and is
+          <a
+            href="https://github.com/kamilanitta/weather"
+            target="_blank"
+          >
+            on GitHub
+          </a>
+          and
+          <a
+            href="https://keen-dragon-344a37.netlify.app/"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+        </p>
+      </footer>
+    </div>
+  )
+}
+
+export default App
